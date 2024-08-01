d5b12abb36153feb04748f1a25e299ba2a82e8fa
hamza ali <hamzaali1997.h@gmail.com>
2024-08-01 09:58:09 -0700

    done

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ const http = require('http');
 const config = require('./config/config');
 const logger = require('./config/logger');
 
+const port = process.env.PORT || config.port || 5000;
+
 const server = http.createServer(app);
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
-  logger.info('Connected to MongoDB');
-  server.listen(config.port, () => {
-    logger.info(`Listening to port ${config.port}`);
+  logger.info('Connected to MongoDB')
+  server.listen(port, () => {
+    logger.info(`Listening to port ${port}`);
   });
 });
+
 
 const exitHandler = () => {
   if (server) {
